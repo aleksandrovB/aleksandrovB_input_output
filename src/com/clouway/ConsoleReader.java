53e0bb10ebaa56e1c655fa0e70a1d61d5fffe269
Bojidar Aleksandrov <bojidar.z.aleksandrov@gmail.com>
2019-04-22 11:53:19 +0300

    Updated ConsoleReader class
    Added documentation to methods
    Changed readChars logic

--- src/com/clouway/ConsoleReader.java
+++ src/com/clouway/ConsoleReader.java
@@ -7,18 +7,39 @@ public class ConsoleReader {
     public ConsoleReader() {
     }
 
+    /**
+     * Reads next line from console
+     * @return next console line
+     */
     public String readString(){
         return scan.nextLine();
     }
 
+    /**
+     * Reads line from console and parses it to Integer
+     * @return next line as integer if next line is number
+     */
     public int readInt() {
         return Integer.parseInt(scan.nextLine());
     }
 
-    public char readChar(){
-        return scan.nextLine().charAt(0);
+    /**
+     * Reads fir
+     * @return
+     */
+    public char readChar() throws Exception {
+        String temp = scan.nextLine();
+        if (temp.length() == 1)
+            return scan.nextLine().charAt(0);
+        else {
+            throw new Exception("This line is not a char");
+        }
     }
 
+    /**
+     * Reads line from console and parses it to float
+     * @return next line as Float if next line is parsable float
+     */
     public float readFloat(){
         return Float.parseFloat(scan.nextLine());
     }
